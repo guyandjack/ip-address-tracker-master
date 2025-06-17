6541662392cc5e5796973dd3e842f3ba0060dc04
guyandjack <guillaume-d@tutanota.com>
2025-06-17 10:40:16 +0200

    update getrequestinfo

--- backend/controller/getRequestInfo.js
+++ backend/controller/getRequestInfo.js
@@ -24,6 +24,8 @@ async function getRequestInfo(req, res) {
     const data = req.body.data;
     const key = process.env.IPFY_API_KEY;
 
+    console.log("data ip for request:", data);
+
     const urlApi =
       type === "ip"
         ? `https://geo.ipify.org/api/v2/country,city?apiKey=${key}&ipAddress=${data}`
